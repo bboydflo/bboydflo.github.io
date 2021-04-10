d1db4d90c4d69957300a423add5137e5512fbda0
bboydflo <florincosmin@fastmail.com>
2021-04-10 18:02:17 +0000

    Deploying to master from @ bboydflo/bboydflo.github.io@ab0156a68dcc12a8df4fb74d732a0479cec26d1a 🚀

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-/**
- * https://nextjs.org/learn/basics/assets-metadata-css/global-styles
- * how to add global styles
- */
-import '../styles/global.css'
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
