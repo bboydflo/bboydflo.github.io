7d6674bc1a8a066318605b574bb2673eed3c72e7
Florin Onciu <florincosmin@fastmail.com>
2021-04-11 12:22:04 +0200

    fixed head in _app and _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 /**
  * https://nextjs.org/learn/basics/assets-metadata-css/global-styles
  * how to add global styles
@@ -5,5 +7,12 @@
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+          <Head>
+            <meta name="viewport" content="viewport-fit=cover" />
+          </Head>
+          <Component {...pageProps} />
+        </>
+    )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -13,11 +13,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head>
-            <meta charSet='UTF-8' />
-            <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
