dab8674be637fb2b85c0804bccb0075c7d8f5455
Florin Onciu <florincosmin@fastmail.com>
2021-04-11 19:27:20 +0200

    changed to single quotes

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
           <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           </Head>
           <Component {...pageProps} />
         </>
