a0b7961e996b7c801568d4f532df51f19a76b7f8
Florin Onciu <florincosmin@fastmail.com>
2021-04-11 19:23:26 +0200

    updated head met tag

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
           <Head>
-            <meta name="viewport" content="viewport-fit=cover" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           </Head>
           <Component {...pageProps} />
         </>
