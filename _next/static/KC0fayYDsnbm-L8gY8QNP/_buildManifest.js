5d8e0a9210d770e31858e52e1f09f4f5765e72e5
bboydflo <florincosmin@fastmail.com>
2021-06-18 00:22:11 +0000

    Deploying to gh-pages from @ bboydflo/bboydflo.github.io@07aaaa1f606f90202154f15298097aa193699fa6 🚀

---
+++ _next/static/KC0fayYDsnbm-L8gY8QNP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-62529fb52bf1e4a862e9.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],"/blog/how-to":["static/chunks/pages/blog/how-to-c0df21e33c254093ac64.js"],sortedPages:["/","/_app","/_error","/blog/how-to"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
