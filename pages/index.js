02afa6fa6ed5b6a60e672a0bb90f5b9929448abf
Florin Cosmin <florincosmin@fastmail.com>
2021-06-23 04:18:07 +0200

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head'
+import { getAllFilesFrontMatter } from '../src/mdx'
 
-export default function Home() {
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }) {
+
+  const MAX_DISPLAY = 5
 
   return (
     <>
@@ -43,7 +52,25 @@ export default function Home() {
         <div className='container px-4 pb-8 mx-auto sm:px-6 md:px-8'>
           <h2 className='mb-4 text-2xl font-semibold text-gray-700'><span className='inline-block pb-1 border-b-4 border-yellow-400'>Latest Blog Posts</span></h2>
 
-          <div className='flex flex-col mb-4 space-y-2'>
+          {!posts.length && <p className=''>No posts found...</p>}
+
+
+          {posts.length && (
+            <div className='flex flex-col mb-4 space-y-2'>
+              {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+                const { slug, date, title, summary, tags } = frontMatter
+                return (
+
+                  <article className='px-2 py-4 transition-all duration-300 rounded-md hover:bg-gray-100' key={slug}>
+                    <div className='text-base leading-6 text-gray-400 uppercase'>{date}</div>
+                    <a href='#' className='inline-block text-2xl leading-8 text-gray-700 transition-all duration-300 transform hover:text-purple-500 hover:translate-x-1'>{title}</a>
+                  </article>
+                )
+              })}
+            </div>
+          )}
+
+          {/* <div className='flex flex-col mb-4 space-y-2'>
             <article className='px-2 py-4 transition-all duration-300 rounded-md hover:bg-gray-100'>
               <div className='text-base leading-6 text-gray-400 uppercase'>May 21, 2021</div>
               <a href='#' className='inline-block text-2xl leading-8 text-gray-700 transition-all duration-300 transform hover:text-purple-500 hover:translate-x-1'>How to share data between browser tabs</a>
@@ -64,7 +91,7 @@ export default function Home() {
               <div className='text-base leading-6 text-gray-400 uppercase'>April 8, 2021</div>
               <a href='#' className='inline-block text-2xl leading-8 text-gray-700 transition-all duration-300 transform hover:text-purple-500 hover:translate-x-1'>TypeScript tips and tricks (part 1)</a>
             </article>
-          </div>
+          </div> */}
 
           <a href='#' className='px-4 py-2 text-xl text-gray-700 transition-all bg-purple-200 border-purple-400 rounded-lg border-1 hover:bg-purple-300 hover:-translate-y-1'>
             Check all blogs
