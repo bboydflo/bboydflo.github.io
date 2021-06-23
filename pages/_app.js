e6591b123e44b483c7b277618d71fac14649317f
Florin Cosmin <florincosmin@fastmail.com>
2021-06-23 02:23:57 +0200

    added meta description

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content='Personal Website of Florin Cosmin Onciu. Here is my personal space where I will mostly post about technology and my personal project'></meta>
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
 
         {/* https://realfavicongenerator.net */}
         <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
@@ -18,8 +21,6 @@ export default function App({ Component, pageProps }) {
         <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
         <link rel='manifest' href='/favicons/site.webmanifest' />
         <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#ffffff' />
 
       </Head>
       <Component {...pageProps} />
